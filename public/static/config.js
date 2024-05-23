74145362d8057d10ef283c0e97788b24e39d01dc
shaoyunlu <shaoyunlu@126.com>
2024-05-23 15:15:35 +0800

    11

--- public/static/config.js
+++ public/static/config.js
@@ -1,2 +1,2 @@
-//const global_base_url = "http://114.116.50.8:8080/"
-const global_base_url = "http://localhost:4000/"
+const global_base_url = "http://114.116.50.8:4000/"
+//const global_base_url = "http://localhost:4000/"
